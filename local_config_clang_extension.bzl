8be9cbc0082799dd6902c74840fe0f56169445c3
Vladimir Antonov <antonovvk@yandex.ru>
2024-09-07 13:45:53 +0300

    Update all deps

--- local_config_clang_extension.bzl
+++
@@ -1,5 +0,0 @@
-load("//:local_config_clang.bzl", "clang_config_rule")
-
-clang_extension = module_extension(
-    implementation = lambda ctx: clang_config_rule(name = "local_config_clang"),
-)
--- update_deps.sh
+++ update_deps.sh
@@ -5,5 +5,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 (
     cd "$SCRIPT_DIR" || exit 1
     (dotnet tool restore && dotnet tool run paket install)
-    bazelisk run @rules_dotnet//tools/paket2bazel:paket2bazel.exe -- --dependencies-file "$(pwd)"/paket.dependencies --output-folder "$(pwd)"
+    bazelisk run @rules_dotnet//tools/paket2bazel -- --dependencies-file "$(pwd)"/paket.dependencies --output-folder "$(pwd)"
 )
